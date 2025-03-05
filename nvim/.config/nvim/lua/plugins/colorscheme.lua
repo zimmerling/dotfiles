7ae74027518de649736b28f114a0ad1792cb4b65
zimmerling <Richard@heinz.localdomain>
2025-03-05 17:12:44 +0100

    FREE: Add transparent backgroud to nvim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,23 @@
-return {
-  -- add gruvbox
-  { "wittyjudge/gruvbox-material.nvim" },
+--return {
+--  -- add gruvbox
+--  { "wittyjudge/gruvbox-material.nvim" },
+--
+--  -- Configure LazyVim to load gruvbox
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "gruvbox-material",
+--    },
+--  },
+--}
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox-material",
-    },
-  },
+return {
+  "tokyonight.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+    }
+  end,
 }
